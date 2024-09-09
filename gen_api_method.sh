f45770043839ad5f371774b281e881e0e7ad7401
Rabbi Hossen Joy <156082447+rabbihossenjoy@users.noreply.github.com>
2024-09-09 15:21:40 +0600

    Update gen_api_method.sh

--- gen_api_method.sh
+++ gen_api_method.sh
@@ -1,25 +1,31 @@
 #!/bin/bash
 
 # Check if at least one argument is provided
-if [ "$#" -lt 2 ]; then
-    echo "Usage: sh gen_api_method.sh <ModelClassName> <LoadingVariableName> [<HttpMethod>]"
+if [ "$#" -lt 1 ]; then
+    echo "Usage: sh gen_api_method.sh <ModelClassName> [<LoadingVariableName>] [<HttpMethod>]"
     exit 1
 fi
 
 # Assign arguments to variables
 MODEL_CLASS_NAME=$1
-LOADING_VAR_NAME=${2:-isLoading}  # Use 'isLoading' if <LoadingVariableName> is not provided
-HTTP_METHOD=${3:-GET}  # Default to GET if not specified
+# If the second argument is POST or GET, treat it as the HTTP method; otherwise, it's the loading variable name
+if [[ "$2" == "POST" || "$2" == "GET" ]]; then
+    LOADING_VAR_NAME="isLoading"
+    HTTP_METHOD=$2
+else
+    LOADING_VAR_NAME=${2:-isLoading} # Default to isLoading if not provided
+    HTTP_METHOD=${3:-GET}            # Default to GET if not provided
+fi
 
 # Convert first letter of class name to lowercase for variable names
-MODEL_VAR_NAME="$(tr '[:upper:]' '[:lower:]' <<< ${MODEL_CLASS_NAME:0:1})${MODEL_CLASS_NAME:1}"
+MODEL_VAR_NAME="$(tr '[:upper:]' '[:lower:]' <<<${MODEL_CLASS_NAME:0:1})${MODEL_CLASS_NAME:1}"
 
-# Determine the correct loading variable name and its version with "Loading" appended
 if [ "$LOADING_VAR_NAME" = "isLoading" ]; then
     LOADING_VAR_NAME_WITH_LOADING="Loading"
 else
     LOADING_VAR_NAME_WITH_LOADING="${LOADING_VAR_NAME}Loading"
 fi
+
 # Generate the body for POST requests if the method is POST
 if [ "$HTTP_METHOD" = "POST" ]; then
     METHOD_LINE="method: HttpMethod.POST,"
@@ -32,9 +38,12 @@ else
 fi
 
 # Create the Dart code
-DART_CODE=$(cat <<EOF
-  final _is$LOADING_VAR_NAME_WITH_LOADING = false.obs;
+DART_CODE=$(
+    cat <<EOF
+
+ final _is$LOADING_VAR_NAME_WITH_LOADING = false.obs;
   bool get is$LOADING_VAR_NAME_WITH_LOADING => _is$LOADING_VAR_NAME_WITH_LOADING.value;
+
 
   late $MODEL_CLASS_NAME _$MODEL_VAR_NAME;
   $MODEL_CLASS_NAME get ${MODEL_VAR_NAME} => _$MODEL_VAR_NAME;
@@ -62,10 +71,10 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     echo "Generated $MODEL_CLASS_NAME Method (macOS)"
 elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
     # Linux (check for xclip or xsel)
-    if command -v xclip &> /dev/null; then
+    if command -v xclip &>/dev/null; then
         echo "$DART_CODE" | xclip -selection clipboard
         echo "Generated $MODEL_CLASS_NAME Method (Linux, xclip)"
-    elif command -v xsel &> /dev/null; then
+    elif command -v xsel &>/dev/null; then
         echo "$DART_CODE" | xsel --clipboard --input
         echo "Generated $MODEL_CLASS_NAME Method (Linux, xsel)"
     else
