bfd7d724273689464a3e2d0495bf0a2c3700eb29
Rabbi Hossen Joy <156082447+rabbihossenjoy@users.noreply.github.com>
2024-10-18 12:11:20 +0600

    Update multi_language.dart

--- multi_language.dart
+++ multi_language.dart
@@ -1,7 +1,26 @@
 #!/bin/bash
 
-# Read the content of strings.dart
-inputText=$(cat lib/languages/strings.dart)
+# Define the URL of the Dart script
+dart_script_url="https://raw.githubusercontent.com/rabbihossenjoy/gen/main/generate_multi_language.dart"
 
-# Run the Dart program with the input text
-dart run test.dart "$inputText"
+# Define the path to the strings.dart file
+strings_file="lib/languages/strings.dart"
+
+# Read the content of strings.dart
+inputText=$(cat "$strings_file")
+
+# Download the Dart script if it doesn't already exist
+if [ ! -f "generate_multi_language.dart" ]; then
+    echo "Downloading the Dart script..."
+    curl -o generate_multi_language.dart "$dart_script_url"
+else
+    echo "Using existing Dart script..."
+fi
+
+# Run the downloaded Dart program with the input text
+dart run generate_multi_language.dart "$inputText"
+
+# Remove the downloaded Dart script after running
+rm -f generate_multi_language.dart
+
+echo "Temporary Dart script has been removed."
